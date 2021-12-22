66ff41ac9c4b94ec6d42ab2bf92bad854fcf6275
fluegelcat <2292009362@qq.com>
2021-12-22 18:59:02 +0800

    Update

---
+++ LabS/simulator/CMakeFiles/lc3simulator.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/fluegelcat/Documents/Code/Simulator/src/main.cpp" "CMakeFiles/lc3simulator.dir/src/main.cpp.o" "gcc" "CMakeFiles/lc3simulator.dir/src/main.cpp.o.d"
+  "/Users/fluegelcat/Documents/Code/Simulator/src/memory.cpp" "CMakeFiles/lc3simulator.dir/src/memory.cpp.o" "gcc" "CMakeFiles/lc3simulator.dir/src/memory.cpp.o.d"
+  "/Users/fluegelcat/Documents/Code/Simulator/src/register.cpp" "CMakeFiles/lc3simulator.dir/src/register.cpp.o" "gcc" "CMakeFiles/lc3simulator.dir/src/register.cpp.o.d"
+  "/Users/fluegelcat/Documents/Code/Simulator/src/simulator.cpp" "CMakeFiles/lc3simulator.dir/src/simulator.cpp.o" "gcc" "CMakeFiles/lc3simulator.dir/src/simulator.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
